f3b1f01457bfa1fa23d996f7fe09720931ae9810
ali <ali-heidari@outlook.com>
2020-04-26 10:16:22 +0430

    Client service for testing connection added

---
+++ pipes-client/src/main/java/com/elpixeler/pipesclient/ClientService.java
@@ -0,0 +1,11 @@
+package com.elpixeler.pipesclient;
+
+/**
+ * Client service to test connection
+ */
+public class ClientService extends Client {
+
+    public ClientService() throws Exception {
+        super("cService");
+    }
+}
